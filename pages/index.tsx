9aaadc60ba1a37e373c0289fd71ccd22a37348a3
kyoh <kyoh@jasonsystem.co.kr>
2024-06-23 11:38:13 +0900

    과제 초기화

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>JASON</title>
+      </Head>
+      <div>
+        이곳에 과제를 작성 하시면 됩니다.
+      </div>
+    </div>
+  )
+}
+
+export default Home
